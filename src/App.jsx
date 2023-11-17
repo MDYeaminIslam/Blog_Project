e935a4af2d08bf312a4b8c5f5fe802a6d698554d
Md. Yeamin Islam <yeamin15-13529@diu.edu.bd>
2023-11-17 19:39:06 +0600

    Home page edited

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       <div className="w-full">
         <Header />
         <main>
-          TODO: <Outlet />
+          <Outlet />
         </main>
         <Footer />
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import appwriteService from '../appwrite/config';
 import { Container, PostCard } from '../components';
+import { login,logout } from '../store/authSlice';
+import { useSelector } from 'react-redux';
 
 function Home() {
   const [posts, setPosts] = useState([]);
+  const user = useSelector((state) => state.auth.userData);
 
   useEffect(() => {
     appwriteService.getPosts().then((posts) => {
@@ -13,21 +16,33 @@ function Home() {
     });
   }, []);
 
-  if (posts.length === 0) {
-    return (
+  return !user ?
+     (
       <div className="w-full py-8 mt-4 text-center">
         <Container>
           <div className="flex flex-wrap">
             <div className="p-2 w-full">
               <h1 className="text-2xl font-bold hover:text-white">
-                Go to ALLPost sections to Read the Posts.
+                Login to read posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    ) : (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-white">
+                Go to all post section to see post.
               </h1>
             </div>
           </div>
         </Container>
       </div>
     );
-  }
+  
   return (
     <div className="w-full py-8">
       <Container>
